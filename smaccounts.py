cdcb9b34c48d1717bd1ac2b7c18b798e81b3562c
Tom Chege <emdeechege@gmail.com>
2018-08-23 14:43:21 +0300

    Created user class and generated readme file

---
+++ smaccounts.py
@@ -0,0 +1,15 @@
+class User:
+    '''
+    Class to create new user accounts and save the same to help in accesssing the pwd locker
+    '''
+
+    def_init_(self,first_name,last_name,password):
+    '''
+    Method to define the properties of the object
+    '''
+    def save_user(self):
+        '''
+        save user details method into users_list
+        '''
+
+        User.users_list.append(self)
---
+++ smaccounts_test.py
@@ -0,0 +1,11 @@
+import unittest # Importing the unittest module
+from user import User
+
+class TestUser(unittest.TestCase):
+    '''
+    Test class that defines test cases for the contact class behaviours
+    Args:
+        unittest.TestCase: Testcase class that helps create test cases
+    '''
+
+    
